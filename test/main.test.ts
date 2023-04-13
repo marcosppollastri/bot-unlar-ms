94b5058e49024f5eab6132904ea6bfa098705972
Marcos J. Peña Pollastri <marcos.p.pollastri@gmail.com>
2023-04-13 00:13:34 -0300

    CBUNLAR-18 coverage en main.ts

---
+++ test/main.test.ts
@@ -0,0 +1,46 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '@src/app.module';
+import { generateSwagger } from '@src/middlewares/swagger';
+import { bootstrap, runAppInProduction } from '@src/main';
+
+jest.mock('@nestjs/core');
+jest.mock('@src/app.module');
+jest.mock('@src/middlewares/swagger');
+
+describe('bootstrap', () => {
+
+
+    beforeEach(() => {
+        jest.clearAllMocks()
+    })
+  it('should bootstrap the application', async () => {
+    const app = {
+      listen: jest.fn(),
+    };
+
+    (NestFactory.create as jest.Mock).mockResolvedValue(app);
+    (generateSwagger as jest.Mock).mockResolvedValue(undefined);
+    app.listen.mockResolvedValue(undefined);
+
+    await bootstrap();
+
+    expect(NestFactory.create).toHaveBeenCalledWith(AppModule);
+    expect(generateSwagger).toHaveBeenCalledWith(app);
+    expect(app.listen).toHaveBeenCalledWith(3000);
+  });
+
+  it('should call bootstrap() when NODE_ENV is not set to "test"', async () => {
+    const app = {
+        listen: jest.fn(),
+      };
+  
+      (NestFactory.create as jest.Mock).mockResolvedValue(app);
+
+    const originalEnv = process.env.NODE_ENV;
+    process.env.NODE_ENV = 'production';
+
+    await runAppInProduction();
+    process.env.NODE_ENV = originalEnv;
+    expect(NestFactory.create).toHaveBeenCalledWith(AppModule);
+  });
+});
